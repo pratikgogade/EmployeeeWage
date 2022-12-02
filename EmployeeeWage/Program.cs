20e5d9f63ae1b7da7588438ce76e549f12bc2b29
unknown <Pratik@Example.com>
2022-12-03 01:31:32 +0530

    [Pratik]Add:Random Num For Attendance

--- EmployeeeWage/Program.cs
+++ EmployeeeWage/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            Random randobj = new Random();
+            int checkPresent = randobj.Next(fullTime);
+            Console.WriteLine("Random number=" + checkPresent);
+            if (checkPresent == fullTime)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}

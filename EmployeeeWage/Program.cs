b3689f339f725258257a88658460a831ed157dcc
unknown <Pratik@Example.com>
2022-12-03 01:34:16 +0530

    [Pratik]Add:Calculate EmpWage

--- EmployeeeWage/Program.cs
+++ EmployeeeWage/Program.cs
@@ -6,18 +6,22 @@ namespace EmployeeeWage
     {
         static void Main(string[] args)
         {
-            int fullTime = 1;
+            int fullTime = 1, Emp_Rate_Per_Hour = 20, empHrs = 0, empwage = 0;
             Random randobj = new Random();
             int checkPresent = randobj.Next(fullTime);
             Console.WriteLine("Random number=" + checkPresent);
             if (checkPresent == fullTime)
             {
+                empHrs = 8;
                 Console.WriteLine("Employee is present");
             }
             else
             {
+                empHrs = 0;
                 Console.WriteLine("Employee is absent");
             }
+            empwage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Employee Wage:" + empwage);
         }
     }
 }

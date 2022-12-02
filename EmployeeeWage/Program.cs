ae1bc4661fc2a069698563ae4c4725eb517b8750
unknown <Pratik@Example.com>
2022-12-03 01:43:52 +0530

    [Pratik]Add:Employee Monthly Wages

--- EmployeeeWage/Program.cs
+++ EmployeeeWage/Program.cs
@@ -6,40 +6,44 @@ namespace EmployeeeWage
     {
         static void Main(string[] args)
         {
-            int fullTime = 1, partTime = 2, Emp_Rate_Per_Hour = 20, empHrs = 0, empwage = 0;
-            Random randobj = new Random();
-            int checkPresent = randobj.Next(0, 3);
-            Console.WriteLine("Random number=" + checkPresent);
-            if (checkPresent == fullTime)
+            int fullTime = 1, partTime = 2, Emp_Rate_Per_Hour = 20, empHrs = 0, empwage = 0, Emp_Working_Days = 20, totalempwage = 0;
+            for (int day = 0; day <= Emp_Working_Days; day++)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is present");
-            }
-            else if (checkPresent == partTime)
-            {
-                empHrs = 4;
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
-            }
-
-            empwage = empHrs * Emp_Rate_Per_Hour;
-            Console.WriteLine("Employee Wage:" + empwage);
-            switch (checkPresent)
-            {
-                case 1:
-                    Console.WriteLine("Employee is fulltime");
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is parttime");
-                    break;
-                default:
+                Random randobj = new Random();
+                int checkPresent = randobj.Next(0, 3);
+                Console.WriteLine("Random number=" + checkPresent);
+                if (checkPresent == fullTime)
+                {
+                    empHrs = 8;
+                    Console.WriteLine("Employee is present");
+                }
+                else if (checkPresent == partTime)
+                {
+                    empHrs = 4;
+                    Console.WriteLine("Employee is present");
+                }
+                else
+                {
+                    empHrs = 0;
                     Console.WriteLine("Employee is absent");
-                    break;
+                }
+                switch (checkPresent)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is fulltime");
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is parttime");
+                        break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                empwage = empHrs * Emp_Rate_Per_Hour;
+                totalempwage += empwage;
+                Console.WriteLine("Employee Wage:" + empwage);
             }
+            Console.WriteLine("Total Wage " + totalempwage);
         }
 
     }
